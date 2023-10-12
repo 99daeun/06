e2eb5bdb30cb1e1cf589347925a38618114442a8
99daeun <lucy8631@naver.com>
2023-10-12 16:06:12 +0900

    3

--- main.c
+++ main.c
@@ -9,3 +9,34 @@ int sumTwo(int a, int b)
 	return output;
 	
 }
+
+int square(int n)
+{
+	return n*n;
+}
+
+int get_max(int x, int y)
+{
+	if (x>y)
+		return x;
+		
+		
+	return y;
+}
+
+int main(int argc, char *argv[])
+{
+	int a,b;
+	a=3;
+	b=10;
+	
+	printf("sumTwo result: %i\n",sumTwo(a,b));
+	
+	printf("square result: %i\n", square(a));
+	
+	printf("get_max result: %i\n", get_max(a,b));
+	
+	
+	
+	return 0;
+}
